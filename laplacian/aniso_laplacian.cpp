33b21ef3e830242f2f8aff4c902171ecc1dbadeb
Vincent Huber <vincent.huber@cemosis.fr>
2015-04-14 11:47:35 +0200

    model: example

---
+++ laplacian/aniso_laplacian.cpp
@@ -0,0 +1,101 @@
+/* -*- mode: c++; coding: utf-8; tab-width: 4; indent-tabs-mode: nil; c-basic-offset: 4; show-trailing-whitespace: t  -*- vim:set fenc=utf-8:ft=tcl:et:sw=4:ts=4:sts=4
+
+   This file is part of the Feel++ library
+
+   Author(s): Christophe Prud'homme <christophe.prudhomme@feelpp.org>
+Date     : Tue Feb 25 11:58:30 2014
+
+Copyright (C) 2014 Feel++ Consortium
+
+This library is free software; you can redistribute it and/or
+modify it under the terms of the GNU Lesser General Public
+License as published by the Free Software Foundation; either
+version 2.1 of the License, or (at your option) any later version.
+
+This library is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public
+License along with this library; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+//! [global]
+#include <feel/feel.hpp>
+//! [include]
+#include <feel/feelmodels/modelproperties.hpp>
+//! [include]
+
+int main(int argc, char**argv )
+{
+  //! [env]
+  using namespace Feel;
+  po::options_description laplacianoptions( "Laplacian options" );
+  laplacianoptions.add_options()
+    ("model", po::value< std::string >()-> default_value("model"), "Name of our model")
+    ("verbose", po::value< bool >()-> default_value( true ), "Display information during execution")
+    ;
+
+  Environment env( _argc=argc, _argv=argv,
+      _desc=laplacianoptions,
+      _about=about(_name="qs_laplacian",
+        _author="Feel++ Consortium",
+        _email="feelpp-devel@feelpp.org"));
+  //! [env]
+
+  //! [load_model]
+  ModelProperties model(soption("model"));
+  map_vector_field<DIM,1,2> bc_u { model.boundaryConditions().getVectorFields<DIM>("velocity","dirichlet") };
+  boost::property_tree::ptree materials; model.materials().getMaterial(materials);
+  //! [load_model]
+  if(boption("verbose") && Environment::isMasterRank() )
+    std::cout << "Model " << soption("model") << " loaded." << std:endl;
+
+  //! [rhs]
+  auto f = expr<DIM,2>( soption(_name="functions.f"), "f" );
+  //! [rhs]
+
+  //! [function_space]
+  auto mesh = loadMesh(_mesh=new Mesh<Simplex<DIM>>);
+  auto Vh = Pch<2>( mesh );
+  auto u = Vh->element();
+  auto v = Vh->element();
+  //! [function_space]
+  
+  //! [forms]
+  auto a = form2( _trial=Vh, _test=Vh);
+  auto l = form1( _test=Vh );
+  //! [forms]
+
+  //! [applying_rhs]
+  l = integrate(_range=elements(mesh),_expr=f*id(v));
+  //! [applying_rhs]
+
+  //! [materials]
+  for(auto it : materials)
+  {
+  a = integrate(_range=elements(mesh),_expr=mu*gradt(u)*trans(grad(v)) );
+  }
+  //! [materials]
+  
+  //! [boundary]
+  for(auto it : bc_u){
+    if(boption("verbose") && Environment::isMasterRank() )
+      std::cout << "[BC] - Applying " << it.second << " on " << it.first << std::endl;
+    a+=on(_range=markedfaces(mesh,it.first), _rhs=l, _element=u, _expr=it.second );
+  }
+  //! [boundary]
+  a.solve(_rhs=l,_solution=u);
+  //# endmarker3 #
+
+  //# marker4 #
+  auto e = exporter( _mesh=mesh );
+  e->addRegions();
+  e->add( "u", u );
+  e->add( "g", v );
+  e->save();
+  return 0;
+  //# endmarker4 #
+}
+//! [global]
